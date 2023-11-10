0cd18e4a5b9dbbf059b5f5b14dd4253a7099b0d1
Matt Batman <batman.matt@gmail.com>
2023-11-10 08:47:35 -0500

    Add home content

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <div>home</div>
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">The best journal app, period.</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          This is the best app for tracking your mood throughout your life. All
+          you have to do is be honest.
+        </p>
+        <div>
+          <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+            get started
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
