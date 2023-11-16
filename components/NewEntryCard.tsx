309ca1ff63d1908dca42bb3b734626927e3ad178
Matt Batman <batman.matt@gmail.com>
2023-11-16 08:13:16 -0500

    Start click handler for new entry

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 const NewEntryCard = () => {
+  const handleOnClick = () => {}
+
   return (
     <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
-      <div className="px-4 py-5 sm:p-6">
+      <div className="px-4 py-5 sm:p-6" onClick={handleOnClick}>
         <span className="text-3xl">New Entry</span>
       </div>
     </div>
