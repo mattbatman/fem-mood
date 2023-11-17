08ebe9e3aacf86694e88eb2caaae1ead76d694bc
Matt Batman <batman.matt@gmail.com>
2023-11-17 09:42:43 -0500

    Add analysis in DB to UI

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -11,6 +11,9 @@ const getEntry = async (id) => {
         id,
       },
     },
+    include: {
+      analysis: true,
+    },
   })
 
   return entry
@@ -18,17 +21,19 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id)
+  const { mood, summary, color, subject, negative } = entry?.analysis
+
   const analysisData = [
     {
       name: 'Summary',
-      value: '',
+      value: summary,
     },
     {
       name: 'Subject',
-      value: '',
+      value: subject,
     },
-    { name: 'Mood', value: '' },
-    { name: 'Negative', value: 'False' },
+    { name: 'Mood', value: mood },
+    { name: 'Negative', value: negative ? 'True' : 'False' },
   ]
 
   return (
@@ -37,7 +42,12 @@ const EntryPage = async ({ params }) => {
         <Editor entry={entry} />
       </div>
       <div className="border-l border-black/10">
-        <div className="bg-blue-300 px-6 py-10">
+        <div
+          className="px-6 py-10"
+          style={{
+            backgroundColor: color,
+          }}
+        >
           <h2 className="text-2xl">Analysis</h2>
         </div>
         <div>
