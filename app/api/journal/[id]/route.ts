48fd0e408d068450492801e2ae5fdfabe3c0a16a
Matt Batman <batman.matt@gmail.com>
2023-11-20 08:46:35 -0500

    Attempt to show analysis in UI

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,6 +1,7 @@
 import { analyze } from '@/utils/ai'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
+import { revalidatePath } from 'next/cache'
 import { NextResponse } from 'next/server'
 
 export const PATCH = async (request: Request, { params }) => {
@@ -21,7 +22,7 @@ export const PATCH = async (request: Request, { params }) => {
 
   const analysis = await analyze(updatedEntry.content)
 
-  await prisma.analysis.upsert({
+  const updated = await prisma.analysis.upsert({
     where: {
       entryId: updatedEntry.id,
     },
@@ -32,5 +33,5 @@ export const PATCH = async (request: Request, { params }) => {
     },
   })
 
-  return NextResponse.json({ data: updatedEntry })
+  return NextResponse.json({ data: { ...updatedEntry, analysis: updated } })
 }
