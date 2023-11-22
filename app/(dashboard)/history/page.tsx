39f0de44a4ae3812f82d438b232e0288f3d553db
Matt Batman <batman.matt@gmail.com>
2023-11-22 09:14:42 -0500

    Associate user and analysis and entries

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,3 +1,11 @@
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getData = async () => {
+  const user = await getUserByClerkID()
+  const analyses = await prisma.analysis.findMany({})
+}
+
 const History = () => {
   return <div>history</div>
 }
